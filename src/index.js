996b73dafa28586326f481deee351e06f54b45f4
KNSTNMarkevich <konstantinmarkevich@gmail.com>
2020-10-01 14:57:15 +0300

    fix: node version

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ exports.max = function max (array) {
   return !array || array.length === 0 ? 0 : array.reduce((a, b) => a > b ? a : b);
 }
 
+
 exports.avg = function avg (array) {
   return !array || array.length === 0 ? 0 : array.reduce((a, b) => (a + b)) / array.length;
 }
